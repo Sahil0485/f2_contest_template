aeb1d3ab3f6022f790c710abb96d3c29b4327b5c
SAHIL THAKUR <sahil20120067926thakur@gmail.com>
2025-08-23 15:27:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,40 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((a) => {
+    if (a.profession == "developer") {
+      console.log(a);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(elm => {
+    if (elm.profession == "developer") console.log(elm);
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmp = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmp);
+  console.log(arr[3]);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((emp) => emp.profession != "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 5, name: "abc", age: "21", profession: "developer" },
+    { id: 6, name: "def", age: "23", profession: "developer" },
+    { id: 7, name: "xyz", age: "25", profession: "admin" },
+  ];
+  arr.push(...arr2);
+  console.log(arr);
 }
